52715483c12771af95b6dc94972772dbec3c4c32
Mana Kashuk <140280356+ManaKashuk@users.noreply.github.com>
2025-07-22 15:07:02 -0500

    Update rbl_gpt4_app.py

--- rbl_gpt4_app.py
+++ rbl_gpt4_app.py
@@ -16,7 +16,7 @@ st.markdown("<h2 style='text-align: left; margin-top: -20px;'>Rice RBLgpt</h1>",
 st.markdown("<h5 style='text-align: left; margin-top: -10px;'>Smart Assistant for Pre- & Post-Award Support at Rice Biotech LaunchPad</h4>", unsafe_allow_html=True)
 
 # Sidebar for selecting category
-category = st.sidebar.selectbox("Select Category", ["Pre-Award", "Post-Award"])
+category = st.sidebar.radio("Select Category", ["Pre-Award", "Post-Award"])
 
 # Question Input
 question = st.text_input("🔍 Ask Rice RBLgpt a question:")
