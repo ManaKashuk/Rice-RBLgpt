8c8c41daa0738b78a19100dbc6e0030513cd89c6
Mana Kashuk <140280356+ManaKashuk@users.noreply.github.com>
2025-07-22 14:41:44 -0500

    Update rbl_gpt4_app.py

--- rbl_gpt4_app.py
+++ rbl_gpt4_app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Rice RBLgpt", layout="centered")
 logo = Image.open("RBLgpt logo.png")
 
 # Display the logo with fixed width and no extra white space
-st.image(logo, width=250)  # Adjust width as needed to match the text
+st.image(logo, width=500)  # Adjust width as needed to match the text
 
 # Tighten the spacing between logo and title
 st.markdown("<h2 style='text-align: left; margin-top: 0px;'>Rice RBLgpt</h1>", unsafe_allow_html=True)
