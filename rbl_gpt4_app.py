7f6df71d6247ab5fd027465dad32beb184f2cf3b
Mana Kashuk <140280356+ManaKashuk@users.noreply.github.com>
2025-07-23 15:26:06 -0500

    Update rbl_gpt4_app.py

--- rbl_gpt4_app.py
+++ rbl_gpt4_app.py
@@ -11,7 +11,7 @@ logo = Image.open("RBLgpt logo.png")
 st.image(logo, width=100)
 
 # Title and subtitle
-st.markdown("<h2 style='text-align: left; margin-top: -20px;'>Rice RBLPgpt</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: left; margin-top: -20px;'>Rice RBLgpt</h2>", unsafe_allow_html=True)
 st.markdown("<h5 style='text-align: left; margin-top: -10px;'>Smart Assistant for Pre- & Post-Award Support at Rice Biotech LaunchPad</h5>", unsafe_allow_html=True)
 
 # Role priming banner
