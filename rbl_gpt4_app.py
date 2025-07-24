6ea54d77b84305ac1d8e16483e61933b80a9b806
Mana Kashuk <140280356+ManaKashuk@users.noreply.github.com>
2025-07-24 10:09:14 -0500

    Update rbl_gpt4_app.py

--- rbl_gpt4_app.py
+++ rbl_gpt4_app.py
@@ -23,7 +23,7 @@ st.markdown(
         <img src='data:image/png;base64,{logo_base64}' width='100'/>
         <h2>Rice RBLPgpt</h2>
         <h5><i>Smart Assistant for Pre- & Post-Award Support at Rice Biotech LaunchPad<h5>
-        <h5>🧠 Trained to respond like a Rice Biotech LaunchPad Research Admin based on SOP guidance.</i><h5>
+        <h6>🧠 Trained to respond like a Rice Biotech LaunchPad Research Admin based on SOP guidance.</i><h6>
     </div>
     """,
     unsafe_allow_html=True
